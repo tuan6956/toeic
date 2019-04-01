9943017a0b8220749a4be41a3c5278a0b7e3b600
JBThong <1512555@student.hcmus.edu.vn>
2019-04-02 03:27:46 +0700

    refactor for blank question

--- ActualTestService/models/index.js
+++ ActualTestService/models/index.js
@@ -4,9 +4,11 @@ const listAnswerOfPart6Model = require('./question_of_part6');
 const paragraphPart7Model = require('./paragraph_part7');
 const questionOfPart7Model = require('./question_of_part7');
 const listeningPar1Model = require('./listening_part1');
+import BlankQuestion from './blank_question';
 
+var blankQuestion = new BlankQuestion();
 module.exports = {
-    blankQuestionModel,
+    blankQuestionModel: blankQuestion,
     blankParagraphModel,
     listAnswerOfPart6Model,
     paragraphPart7Model,
