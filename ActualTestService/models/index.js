c52bfbf74572a80efcb75464e1024dd71dd8af54
JBThong <1512555@student.hcmus.edu.vn>
2019-04-04 16:38:49 +0700

    api import and get all for listening part 2

--- ActualTestService/models/index.js
+++ ActualTestService/models/index.js
@@ -4,6 +4,7 @@ const listAnswerOfPart6Model = require('./question_of_part6');
 const paragraphPart7Model = require('./paragraph_part7');
 const questionOfPart7Model = require('./question_of_part7');
 const listeningPar1Model = require('./listening_part1');
+const listeningPart2Model = require('./listening_part2');
 import BlankQuestion from './blank_question';
 
 var blankQuestion = new BlankQuestion();
@@ -14,4 +15,5 @@ module.exports = {
     paragraphPart7Model,
     questionOfPart7Model,
     listeningPar1Model,
+    listeningPart2Model
 }
