1df5f2e0b6210bd2ff7623340e01421f2ec20370
JBThong <1512555@student.hcmus.edu.vn>
2019-05-12 15:39:33 +0700

    open port of server

--- ActualTestService/configs/db.js
+++ ActualTestService/configs/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // url: "mongodb://103.114.107.16:27017",
-    url: "mongodb://127.0.0.1:27017",
+    url: "mongodb://103.114.107.16:27017",
+    // url: "mongodb://127.0.0.1:27017",
     name: "Toeic",
     collections: {
         user: "User",
