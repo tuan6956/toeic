20077b4e3fcba09ac65611c8b5769e55d68e6c85
JBThong <1512555@student.hcmus.edu.vn>
2019-05-29 00:46:08 +0700

    add host

--- ActualTestService/configs/db.js
+++ ActualTestService/configs/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // url: "mongodb://toeic_db:123456zxC@103.114.107.16:27017/Toeic",
+    url: "mongodb://toeic_db:123456zxC@103.114.107.16:27017/Toeic",
     url: "mongodb://127.0.0.1:27017",
     name: "Toeic",
     collections: {
